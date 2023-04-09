abb293b789106d5b87aaa8efc5aa798bc6001c84
Amal ABDALLAH <amal.abdallah09@gmail.com>
2023-04-10 00:09:38 +0200

    Croquis flags/options d'execution

--- src/demo.c
+++ src/demo.c
@@ -12,7 +12,7 @@ static Parameters params = {
 		.height = 512,
 	},
 	.gen = {
-		.enabled = true,
+		.enabled = false,
 		.shape = CERCLE,
 		.rayon = 250,
 		.nb_points = 1250,
@@ -26,7 +26,76 @@ static Parameters params = {
 	},
 };
 
+void options_gen(){
+	int choix;
+	printf("Quel forme prendra la génération ?\n0 : carré\n1 : cercle\n");
+	scanf("%d", &choix);
+	params.gen.shape = choix == 1 ?  CERCLE : CARRE;
+	
+	printf("Rayon de génération\n");
+	scanf("%d", &params.gen.rayon);
+
+	printf("Nombre de points\n");
+	scanf("%d", &params.gen.nb_points);
+
+	printf("Animation ?\n0: Non\n1: Oui\n");
+	scanf("%d", &choix);
+	params.gen.animation = choix==1 ? true : false;
+
+	return;
+}
+
+void flags(int argc, char* argv[]){
+	int opt, options_index = 0;
+	static struct option long_options[] = {
+            // {"width",  required_argument, 0,  'w' },
+            // {"height",  required_argument, 0,  'h' },
+			{"fenetre",  required_argument, 0,  'f' },
+            {"gen",    no_argument, 0,  'g' },
+			{"maxpar",    required_argument, 0,  'm' },
+			{"taillemin",    required_argument, 0,  't' },
+            {0,         0,                 0,  0 }
+        };
+
+	while ((opt = getopt_long_only(argc, argv, "gw:h:m:t:", long_options, &options_index)) != -1){
+		switch (opt){
+			case 'g':
+				params.gen.enabled = true;
+				options_gen();
+				break;
+
+			case 'w':
+				params.window.width = atoi(optarg);
+				break;
+			
+			case 'h':
+				params.window.height = atoi(optarg);
+				break;
+
+			case 'f':
+				params.window.width = atoi(optarg);
+				params.window.height = params.window.width;
+				break;
+
+			case 'm':
+				params.feuille.max_particules = atoi(optarg);
+				break;
+
+			case 't':
+				params.feuille.taille_min = atoi(optarg);
+				break;
+
+			case '?':
+            break;
+
+        	default:
+            printf("?? getopt returned character code 0%o ??\n", opt);
+		}
+	}
+}
+
 int main(int argc, char* argv[]) {
+	flags(argc, argv);
     MLV_create_window("QuadTree", "", params.window.width, params.window.height);
     SCN_Quadtree(params);
     MLV_free_window();
