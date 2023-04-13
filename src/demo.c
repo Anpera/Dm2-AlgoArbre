8308487298bc0999d9d977159d4b18d6442fa32d
Amal ABDALLAH <amal.abdallah09@gmail.com>
2023-04-13 14:31:19 +0200

    flags TERMINER yaaaaay

--- src/demo.c
+++ src/demo.c
@@ -3,6 +3,7 @@
 #include <stdbool.h>
 #include <MLV/MLV_all.h>
 #include <getopt.h>
+#include <string.h>
 #include "args.h"
 #include "scenes.h"
 
@@ -16,35 +17,16 @@ static Parameters params = {
         .shape = CERCLE,
         .rayon = 250,
         .nb_points = 1250,
-        .concentration = 2,
-        .progressif = true,
+        .concentration = 1.8,
+        .progressif = false,
         .animation = false,
-        .velocite = 2,
+        .velocite = 0,
     },
     .feuille = {
         .max_particules = 5,
         .taille_min = 8,
     },
 };
-
-void options_gen(){
-    int choix;
-    printf("Quel forme prendra la génération ?\n0 : carré\n1 : cercle\n");
-    scanf("%d", &choix);
-    params.gen.shape = choix == 1 ?  CERCLE : CARRE;
-    
-    printf("Rayon de génération\n");
-    scanf("%d", &params.gen.rayon);
-
-    printf("Nombre de points\n");
-    scanf("%d", &params.gen.nb_points);
-
-    printf("Animation ?\n0: Non\n1: Oui\n");
-    scanf("%d", &choix);
-    params.gen.animation = choix==1 ? true : false;
-
-    return;
-}
 
 int count_bits1(int test){
     return __builtin_popcountll(test);
@@ -53,41 +35,96 @@ int count_bits1(int test){
 void flags(int argc, char* argv[]){
     int opt, options_index = 0;
     static struct option long_options[] = {
-            // {"width",  required_argument, 0,  'w' },
-            // {"height",  required_argument, 0,  'h' },
             {"fenetre",  required_argument, 0,  'f' },
             {"gen",    no_argument, 0,  'g' },
+            {"shape", required_argument, 0, 's'},
+            {"rayon", required_argument, 0, 'r'},
+            {"nbpoints", required_argument, 0, 'n'},
+            {"concentration", required_argument, 0, 'c'},
+            {"progressif", required_argument, 0, 'p'},
+            {"animation", no_argument, 0, 'a'},
+            {"velocite", no_argument, 0, 'v'},
             {"maxpar",    required_argument, 0,  'm' },
             {"taillemin",    required_argument, 0,  't' },
             {0,         0,                 0,  0 }
         };
 
-    while ((opt = getopt_long_only(argc, argv, "gw:h:m:t:", long_options, &options_index)) != -1){
+    while ((opt = getopt_long_only(argc, argv, "f:gs:r:n:c:p:av:m:t:", long_options, &options_index)) != -1){
         switch (opt){
-            case 'g':
-                params.gen.enabled = true;
-                options_gen();
-                break;
-
-            case 'w':
-                params.window.width = atoi(optarg);
-                break;
-            
-            case 'h':
-                params.window.height = atoi(optarg);
-                break;
-
             case 'f':
                 params.window.width = atoi(optarg);
-                params.window.height = params.window.width;
+                if (count_bits1(params.window.width) == 1)
+                    params.window.height = params.window.width;
+                else
+                    params.window.width = 512;
+                break;
+
+            case 'g':
+                params.gen.enabled = true;
+                break;
+
+            case 's':
+                if (strcmp(optarg, "carre") == 0)
+                    params.gen.shape = CARRE;
+
+                else if (strcmp(optarg, "cercle") == 0)
+                    params.gen.shape = CERCLE;
+
+                else
+                    printf("Erreur, seul les arguments \"cercle\" et \"carre\" "
+                           "sont acceptés avec le flag -s\n");
+                params.gen.enabled = true;
+                break;
+
+            case 'r':
+                params.gen.rayon = atoi(optarg);
+                if (!params.gen.rayon)
+                    params.gen.rayon = 250;
+                params.gen.enabled = true;
+                break;
+
+            case 'n':
+                params.gen.nb_points = atoi(optarg);
+                if (!params.gen.nb_points)
+                    params.gen.nb_points = 1250;
+                params.gen.enabled = true;
+                break;
+
+            case 'c':
+                params.gen.concentration = atof(optarg);
+                if (!params.gen.concentration)
+                    params.gen.concentration = 1.8;
+                params.gen.enabled = true;
+                break;
+
+            case 'p':
+                if (atoi(optarg) == 1)
+                    params.gen.progressif = true;
+                else
+                    params.gen.progressif = false;
+                params.gen.enabled = true;
+                break;
+
+            case 'a':
+                params.gen.animation = true;
+                params.gen.enabled = true;
+                break;
+
+            case 'v':
+                params.gen.velocite = atoi(optarg);
+                params.gen.enabled = true;
                 break;
 
             case 'm':
                 params.feuille.max_particules = atoi(optarg);
+                if (params.feuille.max_particules == 0)
+                    params.feuille.max_particules = 5;
                 break;
 
             case 't':
                 params.feuille.taille_min = atoi(optarg);
+                    if (count_bits1(params.feuille.taille_min) != 1)
+                        params.feuille.taille_min = 8;
                 break;
 
             case '?':
